9bec03000a1f6a96361e3f3c7f5e093d9153e79d
Damir <115581748+Damir-p@users.noreply.github.com>
2023-07-30 14:03:22 +0600

    Update urls.py

--- Bot_service/urls.py
+++ Bot_service/urls.py
@@ -26,12 +26,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API",
+        title="API",
         default_version='v1',
         description="API description",
-        terms_of_service="https://www.yourapp.com/terms/",
-        contact=openapi.Contact(email="contact@yourapp.com"),
-        license=openapi.License(name="Your License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
